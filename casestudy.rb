537511758c1b9e8d24d269e70b2393f72c324d59
sandipsam <37140069+sandipsam@users.noreply.github.com>
2018-03-07 17:27:31 +0530

    updated working code without ranking
    updated working code without ranking

--- casestudy.rb
+++ casestudy.rb
@@ -1,5 +1,5 @@
 require 'csv'
-
+=begin
 dataHash = CSV.read("Customers.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
 
 hashed_data = dataHash.map { |d| d.to_hash }
@@ -13,37 +13,38 @@ CSV.foreach("Customers.csv", { encoding: "UTF-8", headers: true, header_converte
 end
 
 puts dataArray
-
+=end
 
 ###############################################
 
 custArray = Array.new
+custID = ""
+innercustID = ""
 
 CSV.open('output.csv', 'w') do |csv|
 	csv << ['customerID', 'total sales', 'rank']
 	i = 0
 	
-	CSV.foreach('Customers.csv') do |row|
+	CSV.foreach('Customers.csv', { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
 	    amount = 0
 	    i = i + 1
-	    custID << row[10]
+	    custID = row[10]
 	    
 	    
-	    if custArray.include?(custID) = false then
-		    puts "processing for customer -> #{custtID}"
-
+	    if custArray.include?(custID) == false then
+		   
 		    amount = amount + row[4].to_f
 		    j = 0
-		    CSV.foreach('Customers.csv') do |innerrow|
-
+		    CSV.foreach('Customers.csv', { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |innerrow|
+			
 			j = j+1
-			innercustID << row[10]
-
-			if innercustID = custID then
+			innercustID = innerrow[10]
+			
+			if innercustID == custID then
 				if i != j then
-					amount = amount + row[4].to_f
+					amount = amount + innerrow[4].to_f
 				end
-			end if
+			end
 		    end 	
 
 		    puts "Customer id ==> #{custID} and total amount = #{amount}"
